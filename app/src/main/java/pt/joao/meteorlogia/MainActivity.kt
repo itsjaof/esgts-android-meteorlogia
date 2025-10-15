cb6ebec4c3e8ae0acf30438190463e3ea02c48e8
itsjaof <fjoao.quiterio@gmail.com>
2025-10-15 23:13:23 +0100

    Background

--- app/src/main/java/pt/joao/meteorlogia/MainActivity.kt
+++ app/src/main/java/pt/joao/meteorlogia/MainActivity.kt
@@ -8,15 +8,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,6 +66,29 @@ class MainActivity : ComponentActivity() {
                     .size(52.dp)
                     .clip(CircleShape)
             )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun FullComponent() {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(R.drawable._86449326_37c2256216_b),
+                contentDescription = "Foto",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .alpha(0.5f)
+            )
+
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                TopBar()
+            }
         }
     }
 }
